507a22d4babd9b115d9e54e369440064ea35c585
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-18 23:53:10 +0600

    mongodb uri updated

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const sendEmail = (emailAddress, emailData) => {
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 let client;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4qgkjzt.mongodb.net/learnJapaneseDB?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4qgkjzt.mongodb.net/learnJapaneseDB`;
 
 console.log(uri)
 
